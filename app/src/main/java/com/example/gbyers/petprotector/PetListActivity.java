d687fa051e9a28fc184529940f040245ff4f4bc7
Byers <gbyers@student.cccd.edu>
2016-10-25 16:52:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/gbyers/petprotector/PetListActivity.java
@@ -0,0 +1,82 @@
+package com.example.gbyers.petprotector;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.AnyRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class PetListActivity extends AppCompatActivity {
+    //store memebr variable to whatevr has been selected default will be (R.drawable.none)
+    private Uri imageUri;
+    private ImageView selectImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_list);
+        selectImage = (ImageView) findViewById(R.id.petImageView);
+        //constructs Uri to any resource
+        imageUri = getUriToResource(this, R.drawable.none);
+
+        //set image uri to the view
+       // selectImage = setImageUri(imageUri);
+    }
+    public void selectPetImage(View v)
+    {
+        ArrayList<String> permList = new ArrayList<>();
+
+        //start by seeing if we have permisson to camera
+        int camPerm = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if(camPerm != PackageManager.PERMISSION_GRANTED)
+        {
+            permList.add(Manifest.permission.CAMERA);
+        }
+        int readPerm = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(readPerm != PackageManager.PERMISSION_GRANTED)
+            permList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        int writePerm = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(writePerm != PackageManager.PERMISSION_GRANTED)
+            permList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(permList.size() > 0)
+        {
+            //method accepts only String
+            //convert arrayLList to array of String
+            String[] perms = new String[permList.size()];
+
+            ActivityCompat.requestPermissions(this, permList.toArray(perms),100);
+
+        }
+        //if we have all thre permissions. Open Image gallery
+        if(camPerm == PackageManager.PERMISSION_GRANTED&& writePerm == PackageManager.PERMISSION_GRANTED && readPerm == PackageManager.PERMISSION_GRANTED)
+        {
+            //use an intent to launch gallery and take pics
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, 100);
+        }else
+            Toast.makeText(this, "set Permisions",Toast.LENGTH_LONG);
+
+    }
+    public static Uri getUriToResource(@NonNull Context context, @AnyRes int resId)throws Resources.NotFoundException{
+        Resources res = context.getResources();
+        return Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE+
+                "://" + res.getResourcePackageName(resId)+
+                '/'+res.getResourceTypeName(resId)+
+                '/'+res.getResourceEntryName(resId));
+    }
+}
